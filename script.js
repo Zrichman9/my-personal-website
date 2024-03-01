e97bdecb855957f19895eb361eec11d6eb6bcb82
Zachary.Richman <zrichma9@gmail.com>
2024-03-01 14:08:03 -0500

    added click button

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Zachary J Richman";
 document.getElementById("content-container").appendChild(h2);
+
+
+document.getElementById("myBtn").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=9eWERM6QSys&t=30s");
+  });
